b2b1496d467bdae778ffb67ae30714ad2ad3ec1c
Mathias Rodriguez <22502952@me.uvsq.fr>
2026-02-20 15:12:29 +0100

    corrige bug et implémente multiplication

--- pycalc.py
+++ pycalc.py
@@ -1,7 +1,10 @@
 import sys
-if len(sys.argv) == 4 and isinstance(sys.argv[1],float) and isinstance(sys.argv[3],float) : 
-    resultat = 0
+resultat = 0
+if len(sys.argv) == 4 : 
+    n1 = float(sys.argv[1])
+    n2 = float(sys.argv[3])
     
+    if sys.argv[2] == '*' :
+        resultat = n1*n2
 
-
-print("hoho")
+print(resultat)
