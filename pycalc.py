4066572fb58344b5694f0436aadbc263bc244227
Mathias Rodriguez <22502952@me.uvsq.fr>
2026-02-20 15:13:45 +0100

    implémente la division

--- pycalc.py
+++ pycalc.py
@@ -3,8 +3,17 @@ resultat = 0
 if len(sys.argv) == 4 : 
     n1 = float(sys.argv[1])
     n2 = float(sys.argv[3])
-    
+
     if sys.argv[2] == '*' :
         resultat = n1*n2
 
+    elif sys.argv[2] == '/' :
+        resultat = n1/n2
+
+    elif sys.argv[2] == '+' :
+        resultat = n1+n2
+    
+    elif sys.argv[2] == '-' :
+        resultat = n1-n2
+    
 print(resultat)
